322b8fc3e2a8bc474c90fdfd25e4c65eb21693ea
Sai40k <151876367+Sai40k@users.noreply.github.com>
2024-01-16 19:06:09 +0000

    Domain Generator

---
+++ app.js
@@ -0,0 +1,18 @@
+let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dom= ['.com','.io','.es'];
+  
+  for(let p=0;p< pronoun.length; p++)
+  {
+    for(let a=0; a<adj.length; a++)
+      {
+        for(let n=0; n< noun.length; n++)
+        {
+          for(let d=0;d<dom.length;d++)
+          {
+            console.log(pronoun[p]+adj[a]+noun[n]);
+          }
+        }
+      }
+  }
